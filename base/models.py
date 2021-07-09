67cf7078e7ac79604954f05d0b6d65dc0c88a3e6
sahandnlf <74633642+sahandnlf@users.noreply.github.com>
2021-07-09 22:16:44 +0700

    Update models.py

--- base/models.py
+++ base/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    complete = models.BooleanField(default=False)
+    judul = models.CharField(max_length=200)
+    deskripsi = models.TextField(null=True, blank=True)
+    selesai = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
