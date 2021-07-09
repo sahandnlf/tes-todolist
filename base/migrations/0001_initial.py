70c998180bae02ee5111a7611b3a1c310684593c
sahandnlf <74633642+sahandnlf@users.noreply.github.com>
2021-07-09 22:22:05 +0700

    Update 0001_initial.py

--- base/migrations/0001_initial.py
+++ base/migrations/0001_initial.py
@@ -18,9 +18,9 @@ class Migration(migrations.Migration):
             name='Task',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('description', models.TextField(blank=True, null=True)),
-                ('complete', models.BooleanField(default=False)),
+                ('judul', models.CharField(max_length=200)),
+                ('deskripsi', models.TextField(blank=True, null=True)),
+                ('selesai', models.BooleanField(default=False)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
